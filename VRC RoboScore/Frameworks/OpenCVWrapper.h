7fe8d0447ccba359728bc9f6373d102461c3aeac
evanwboyle <evan.wesley.boyle@gmail.com>
2025-07-27 19:45:10 -0400

    Added goal overlay to video scoring

---
+++ VRC RoboScore/Frameworks/OpenCVWrapper.h
@@ -0,0 +1,16 @@
+//
+//  OpenCVWrapper.h
+//  VRC RoboScore
+//
+//  Created by Evan Boyle on 7/27/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OpenCVWrapper : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
